ca20adc40ee008abe09ba82d76c7928bd85c1d5f
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-01-03 01:42:44 +0000

    height of tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	right_h = tree->right ? 1 + binary_tree_height(tree->right): 0;
+	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 
 	height = right_h > left_h ? right_h : left_h;
