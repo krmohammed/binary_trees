828320f3db4d693c70966ae33167f57c2bfd6598
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-01-03 00:50:38 +0000

    betty correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_insert_left - inserts a node as the left child
  * of another node
- * 
+ *
  * @parent: pointer to the node to insert the left-child in
  * @value: value to store in the new node
  *
