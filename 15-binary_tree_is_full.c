b65ed6b6c264a4949735ec343e923f96f0cdb6f7
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-01-05 00:29:04 +0000

    task 15 -> full binary tree

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_full - chekcks if a binary tree if full
+ * @tree: pointer to root node of the tree
+ *
+ * Return: 1 if full, 0 otherwise
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	/*int is_full;*/
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	return (0);
+}
